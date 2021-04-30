ce0f2b20f98537640e7ce89f1ed26cd43566b05b
Zan_88 <7388931+zan-88@users.noreply.github.com>
2021-04-30 19:09:09 -0400

    Completed front end to do

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,26 +1,42 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
-      id:1,
+      id: 1,
       text: 'Donctors Appointment',
       day: 'Feb 5th at 2:30pm',
       reminder: true
     }
   ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = {id, ...task}
+
+    setTasks([...tasks, newTask])
+  }
+
 
   const deleteTask = (id) => {
-    console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   return (
     <div className="container">
-      <Header title="Task Tracker"/>
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      <Header title="Task Tracker" onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
--- to-do-list/src/components/Header.js
+++ to-do-list/src/components/Header.js
@@ -1,18 +1,12 @@
 import Button from './Button'
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log("click")
-    }
-    
+const Header = ({ title, onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello' onClick={onClick}/>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
     )
 }
-
-
 
 export default Header
